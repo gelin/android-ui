6adcdffce15cfd8fa4f28db8f6a85eb3b75ad549
Denis Nelubin <dnelubin@gmail.com>
2013-04-03 10:31:15 +0700

    Added Input Controls example.

---
+++ inputs/src/ru/gelin/android/example/inputs/Inputs.java
@@ -0,0 +1,15 @@
+package ru.gelin.android.example.inputs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Inputs extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inputs);
+    }
+}
