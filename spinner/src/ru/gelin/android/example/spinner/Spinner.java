9a41348a686255ec51ceeef62f6b37e4974bb895
Denis Nelubin <dnelubin@gmail.com>
2013-04-05 21:55:12 +0700

    Added spinner example.

---
+++ spinner/src/ru/gelin/android/example/spinner/Spinner.java
@@ -0,0 +1,15 @@
+package ru.gelin.android.example.spinner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Spinner extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spinner);
+    }
+}
