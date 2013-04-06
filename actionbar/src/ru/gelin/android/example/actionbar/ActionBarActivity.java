3730073c0b32268002a3633d0688159366bf60b6
Denis Nelubin <dnelubin@gmail.com>
2013-04-06 14:25:57 +0700

    Added simple actionbar example

---
+++ actionbar/src/ru/gelin/android/example/actionbar/ActionBarActivity.java
@@ -0,0 +1,33 @@
+package ru.gelin.android.example.actionbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class ActionBarActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actionbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_LONG).show();
+        return super.onOptionsItemSelected(item);
+    }
+
+}
